8bb957229fcbeba39768a275d839ecc685feb1e1
tpola <thomas@polaert.com>
2021-11-26 22:09:48 +0100

    prj creation

--- AD.Test/MainWindow.xaml.cs
+++ AD.Test/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,12 @@ namespace AD.Test
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			using (var domain = Domain.GetComputerDomain())
+				computer_domain.Text = domain.Name;
 		}
 	}
 }
